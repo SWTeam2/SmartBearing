d82fe4a16ddc2d104b30ca3ba0a3e727e6067138
chy0503 <202015006@kyonggi.ac.kr>
2023-07-29 11:07:51 +0900

    Initial commit

---
+++ src/main/java/com/example/smartfactory_bearingrul/SmartFactoryBearingRulApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartfactory_bearingrul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFactoryBearingRulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFactoryBearingRulApplication.class, args);
+    }
+
+}
