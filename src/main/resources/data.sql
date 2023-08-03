c9c345c96143c8d3cc5dd0b6bc27febb2ced630f
choheeyeon <202015006@kyonggi.ac.kr>
2023-08-03 22:52:21 +0900

    #2 - 멤버 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO Employee (employee_id, name, birthday, gender, phone, email, department, position, in_charge)
 VALUES ('1001', '홍길동', '2001-01-01', 'M', '010-0000-0000', 'hgd@gmail.com', '영업', '팀장', 'Bearing_1');
 
+INSERT INTO Member (employee_id, member_id, password, created_at)
+VALUES ('1001', 'hgd', '1234', now())
