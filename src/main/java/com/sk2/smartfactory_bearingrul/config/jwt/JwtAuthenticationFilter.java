cf99d84be690b1b5ae0237f5b85616d9393cc6c4
choheeyeon <202015006@kyonggi.ac.kr>
2023-08-04 10:55:35 +0900

    #2 - fileter GenericFilterBean으로 변경

--- src/main/java/com/sk2/smartfactory_bearingrul/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sk2/smartfactory_bearingrul/config/jwt/JwtAuthenticationFilter.java
@@ -3,68 +3,38 @@ package com.sk2.smartfactory_bearingrul.config.jwt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Autowired
-    private final JwtTokenProvider tokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try {
-            // 요청에서 토큰 가져오기
-            String token = parseBearerToken(request);
-            log.info("Filter is running...");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //헤더에서 토큰 받아오기
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
-            // 토큰 검사하기. JWT이므로 인가 서버에 요청하지 않고도 검증 가능
-            if(token != null && !token.equalsIgnoreCase(null)) {
-                // userId 가져오기. 위조된 경우 예외 처리된다.
-                String userId = tokenProvider.validateAndGetUserId(token);
-                log.info("Authenticated user ID : " + userId);
-
-                //인증 완료. SecurityContextHolder에 등록해야 인증된 사용자라고 생가한다.
-                AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userId, // 인증된 사용자의 정보. 문자열이 아니어도 아무것이나 넣을 수 있다. 보통 UserDetails라는 오브젝트를 넣는데 우리는 넣지 않았다.
-                        null,
-                        AuthorityUtils.NO_AUTHORITIES
-                );
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-                securityContext.setAuthentication(authentication); // 인증 정보 넣기
-                SecurityContextHolder.setContext(securityContext); // 다시 등록
-            }
-        } catch (Exception ex) {
-            logger.error("Could not set user authentication in security context", ex);
+        //토큰이 유효하다면
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
-        filterChain.doFilter(request, response);
-    }
-
-    private String parseBearerToken(HttpServletRequest request) {
-        // Http 요청의 헤더를 파싱해 Bearer 토큰을 리턴한다.
-        String bearerToken = request.getHeader("Authorization");
-
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
+        //다음 Filter 실행
+        chain.doFilter(request, response);
     }
 }
