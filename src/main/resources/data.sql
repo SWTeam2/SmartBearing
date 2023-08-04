2f563743979d000461e61e090a60b0c212f350a2
choheeyeon <202015006@kyonggi.ac.kr>
2023-08-04 11:20:21 +0900

    #2 - init data 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,8 @@ INSERT INTO Employee (employee_id, name, birthday, gender, phone, email, departm
 VALUES ('1001', '홍길동', '2001-01-01', 'M', '010-0000-0000', 'hgd@gmail.com', '데이터 1팀', '관리자', 'Bearing_1');
 INSERT INTO Employee (employee_id, name, birthday, gender, phone, email, department, position, in_charge)
 VALUES ('1002', '홍길순', '2001-01-02', 'M', '010-0000-0001', 'hgs@gmail.com', '데이터 1팀', '사원', null);
+INSERT INTO Employee (employee_id, name, birthday, gender, phone, email, department, position, in_charge)
+VALUES ('1003', '김미미', '2001-01-03', 'F', '010-0000-0002', 'kmm@gmail.com', '데이터 2팀', '사원', null);
 
 -- Member
 INSERT INTO Member (employee_id, member_id, password, created_at)
