d78abac2293bcaf6af6fbca5c87baf1853defd04
choheeyeon <202015006@kyonggi.ac.kr>
2023-08-05 20:37:41 +0900

    #2 - jwt token header 변경

--- src/main/java/com/sk2/smartfactory_bearingrul/util/jwt/JwtTokenProvider.java
+++ src/main/java/com/sk2/smartfactory_bearingrul/util/jwt/JwtTokenProvider.java
@@ -25,6 +25,7 @@ public class JwtTokenProvider {
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+
     private final long tokenValidTime = 30 * 60 * 1000L; // 토큰 유효시간 = 30분
     private final LoginMemberService loginMemberService;
 
@@ -69,6 +70,6 @@ public class JwtTokenProvider {
 
     // Request의 Header에서 token 값 가져오기
     public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("Authorization");
+        return request.getHeader("X-AUTH-TOKEN");
     }
 }
