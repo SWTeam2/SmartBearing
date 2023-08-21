5514e87a4479550117ac19896f9364a879729dfe
choheeyeon <202015006@kyonggi.ac.kr>
2023-08-21 21:24:49 +0900

    #24 - 대시보드에서 new 알림 확인하기 구현

--- src/main/java/com/sk2/smartfactory_bearingrul/controller/NotificationController.java
+++ src/main/java/com/sk2/smartfactory_bearingrul/controller/NotificationController.java
@@ -5,18 +5,19 @@ import com.sk2.smartfactory_bearingrul.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/dashboard/notification")
+@RequestMapping("/api/notification")
 @RestController
 public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @GetMapping
-    public ResponseEntity<List<NotificationDto>> getAllNotifications() {
-        List<NotificationDto> notifications = notificationService.getAllNotifications();
-        return ResponseEntity.ok(notifications);
+    @GetMapping("/countAfter/{createdAt}")
+    public ResponseEntity<Long> getCountByCreatedAt(@PathVariable String createdAt) {
+        return ResponseEntity.ok(notificationService.getCountNewNoti(createdAt));
     }
 }
--- src/main/java/com/sk2/smartfactory_bearingrul/service/NotificationService.java
+++ src/main/java/com/sk2/smartfactory_bearingrul/service/NotificationService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,4 +56,7 @@ public class NotificationService {
     }
 
 
+    public long getCountNewNoti(String createdAt) {
+        return notificationRepository.countByCreatedAtAfter(LocalDateTime.parse(createdAt.replace("Z", "")));
+    }
 }
